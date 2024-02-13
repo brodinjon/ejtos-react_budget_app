779b1779813fb5c4d5568134c7ecf41b8b9a25aa
brodinjon <66535129+brodinjon@users.noreply.github.com>
2024-02-13 08:28:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Budget from './components/Budget';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
-import RemainingBudget from './components/Remaining';
+import Remaining from './components/Remaining';
 import Currency from './components/Currency';
 
 const App = () => {
@@ -19,7 +19,7 @@ const App = () => {
                         <Budget />
                     </div>
                     <div className='col-sm'>
-                        <RemainingBudget />
+                        <Remaining />
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
